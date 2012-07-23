bcf0b0c5ee822386b5b19b90420d7cf52cbec6d7
shae ponzio <defiancegaming99@gmail.com>
2012-07-22 19:07:17 -0700

    javalearning

---
+++ src/com/github/ShaeSoFly/javalearning/javamain.java
@@ -0,0 +1,43 @@
+package com.github.ShaeSoFly.javalearning;
+
+import java.util.Scanner;
+
+public class javamain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		System.out.println("hello world");
+			 //Scanner sc = new Scanner(System.in);
+		     //String apple = sc.nextLine();
+		     //System.out.println(apple); 
+		     int number1;
+		     number1=56;
+		     number1=75;
+		     int number2;
+		     number2=13;
+		     int number3;
+		     number3=number1 + number2;
+		     
+		     
+		     System.out.println("number1 is" +number1);
+		     System.out.println("number2 is" +number2);
+		     System.out.println("number3 is" +number3);
+		     
+		     String text1;
+		     text1="idshfkdsa";
+		     String text2;
+		     text2="hey";
+		     String text3;
+		     text3=text1 + text2;
+		     
+		     System.out.println("text1 is " +text1);
+		     System.out.println("text2 is " +text2);
+		     System.out.println("text3 is " +text3);
+		     
+		    
+				     }
+
+}
